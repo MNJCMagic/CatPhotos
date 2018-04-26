b899ee23d694f22b367e82ecef5fcedb4d75afb9
Mike Cameron <mike@mikecameron.tv>
2018-04-26 14:59:29 -0400

    cat photo objects initialized and stored in array

--- CatPhotos/ViewController.m
+++ CatPhotos/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "PhotoObject.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) NSArray* dataObjects;
+@property (nonatomic, strong) NSMutableArray<PhotoObject*>* photoObjects;
 
 @end
 
@@ -16,14 +19,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=671446b75c6c30cc03fd61ca2dc63cf1&tags=cat"];
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"error %@", error.localizedDescription);
+            return;
+        }
+        [self parseResponseData:data];
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // This will run on the main queue
+            //            [self.tableView reloadData];
+        }];
+    }];
+    [dataTask resume];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)parseResponseData:(NSData*) data {
+    NSError *jsonError = nil;
+    NSDictionary *results = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+    if (jsonError) {
+        NSLog(@"jsonError: %@", jsonError.localizedDescription);
+        return;
+    }
+    NSDictionary *secondResults = [NSDictionary dictionaryWithDictionary:results[@"photos"]];
+    NSArray *photos = [NSArray arrayWithArray:secondResults[@"photo"]];
+    self.dataObjects = photos;
+    for (NSDictionary* dict in self.dataObjects) {
+        PhotoObject *tempObj = [[PhotoObject alloc] init];
+        tempObj.name = dict[@"title"];
+        NSString *urlString = [NSString stringWithFormat:@"https://farm%@.staticflickr.com/%@/%@_%@.jpg", dict[@"farm"], dict[@"server"], dict[@"id"], dict[@"secret"]];
+        NSURL *photoURL = [NSURL URLWithString:urlString];
+        NSData *data = [NSData dataWithContentsOfURL:photoURL];
+        UIImage *image = [UIImage imageWithData:data];
+        tempObj.image = image;
+        [self.photoObjects addObject:tempObj];
+        NSLog(@"added");
+        
+    };
 }
-
-
-@end
+    
+    
+    
+    
+    @end
